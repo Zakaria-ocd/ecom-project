21189cbc74c938ec824d154c370021b54b41cbfb
Zakaria-ocd <156421881+Zakaria-ocd@users.noreply.github.com>
2024-12-30 21:17:20 +0100

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
-import logo from "@/../public/assets/logo.png";
 
 export default function Logo({ className = "" }) {
-  return <Image src={logo} alt="logo" className={className} />;
+  return <Image src={"/assets/logo.png"} alt="logo" className={className} />;
 }
